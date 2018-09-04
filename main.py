d1e5ea9a55a81baeb448b03182584811eb3405a5
imaharu <light1200out@gmail.com>
2018-09-04 17:54:33 +0900

    リファクタ

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from create_dict import *
 from create_complete_corpus import *
 # get_simple_data('iwanami.txt')
 # get_dict('uniq_complete.corpus.iwanami','uniq_complete.corpus.tikuma')
-create_complete_corpus('uniq_complete.corpus.iwanami','uniq_complete.corpus.tikuma')
+# create_complete_corpus('uniq_complete.corpus.iwanami','uniq_complete.corpus.tikuma')
